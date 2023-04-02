f1f41477d8930c17e8f96c29cd84346fb88f6798
baz1232w <yermak12324@gmail.com>
2023-04-02 18:14:11 +0300

    complete ex_5

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const textArea = document.querySelector('.text_area'),
       total = document.querySelector('.total'),
-      character = document.querySelector('.character');
+      character = document.querySelector('.character'),
+      list = document.querySelector('.list')
 
 textArea.addEventListener('blur',()=>{
-    const arr  = textArea.value.split(' ')
+    const arr1  = textArea.value.split(' ')
+    const arr2 = textArea.value.split('')
     let totalWords = 0;
     let totalCharacter = 0;
-    for(const word of arr ){
+    let obj = {}
+    let objTotal = 0;
+    arr2.forEach(char => {
+        if(char !== ' '){
+            obj[char] = obj[char] + 1 || 1 
+            objTotal++
+        }
+    })
+
+    if(list.lastChild){
+        list.querySelectorAll('li').forEach(el => list.removeChild(el))
+    }
+
+    for(let key in obj){
+        const li = document.createElement('li')
+        li.textContent = key + " " + ((obj[key] / objTotal) * 100).toFixed(2) + " %"
+
+        list.appendChild(li)
+    }
+
+
+    for(const word of arr1 ){
         if(word.length > 0){
             totalWords++
             for(let char of word){
