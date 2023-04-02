68cc5795c9fab6231d5cbdce7a4cd1331e90be0e
baz1232w <yermak12324@gmail.com>
2023-04-02 17:12:26 +0300

    complete ex_4

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const textArea = document.querySelector('.text_area'),
-      total = document.querySelector('.total');
+      total = document.querySelector('.total'),
+      character = document.querySelector('.character');
 
 textArea.addEventListener('blur',()=>{
     const arr  = textArea.value.split(' ')
-    let result = 0;
-
+    let totalWords = 0;
+    let totalCharacter = 0;
     for(const word of arr ){
         if(word.length > 0){
-            result++
+            totalWords++
+            for(let char of word){
+                totalCharacter++
+            }
         }else{
             false
         }
     }
 
-    total.textContent = result
+    character.textContent = totalCharacter
+    total.textContent = totalWords
 })
