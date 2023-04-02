9cfa7abcce09b6d09df5f8828efd7500ed25ab54
baz1232w <yermak12324@gmail.com>
2023-04-02 18:30:09 +0300

    complete ex_6

--- index.js
+++ index.js
@@ -1,45 +1,57 @@
 const textArea = document.querySelector('.text_area'),
       total = document.querySelector('.total'),
       character = document.querySelector('.character'),
-      list = document.querySelector('.list')
+      list = document.querySelector('.list'),
+      totalWordsCheckBox = document.getElementById('totalWords'),
+      totalCharacterCheckBox = document.getElementById('totalCharecter'),
+      percentCheckBox = document.getElementById('percentCheckBox');
 
 textArea.addEventListener('blur',()=>{
     const arr1  = textArea.value.split(' ')
-    const arr2 = textArea.value.split('')
     let totalWords = 0;
     let totalCharacter = 0;
-    let obj = {}
-    let objTotal = 0;
-    arr2.forEach(char => {
-        if(char !== ' '){
-            obj[char] = obj[char] + 1 || 1 
-            objTotal++
+    if(percentCheckBox.checked){
+        const arr2 = textArea.value.split('')
+        let obj = {}
+        let objTotal = 0;
+   
+        arr2.forEach(char => {
+            if(char !== ' '){
+                obj[char] = obj[char] + 1 || 1 
+                objTotal++
+           }
+        })
+    
+        if(list.lastChild){
+            list.querySelectorAll('li').forEach(el => list.removeChild(el))
         }
-    })
+        
+        for(let key in obj){
+            const li = document.createElement('li')
+            li.textContent = key + " " + ((obj[key] / objTotal) * 100).toFixed(2) + " %"
 
-    if(list.lastChild){
-        list.querySelectorAll('li').forEach(el => list.removeChild(el))
+            list.appendChild(li)
+        }
+    }
+    
+
+    if(totalWordsCheckBox.checked){
+        for(const word of arr1 ){
+            if(word.length > 0){
+                totalWords++
+            }
+        }
     }
 
-    for(let key in obj){
-        const li = document.createElement('li')
-        li.textContent = key + " " + ((obj[key] / objTotal) * 100).toFixed(2) + " %"
-
-        list.appendChild(li)
-    }
-
-
+    if(totalCharacterCheckBox.checked)
     for(const word of arr1 ){
         if(word.length > 0){
-            totalWords++
             for(let char of word){
                 totalCharacter++
             }
-        }else{
-            false
         }
     }
 
-    character.textContent = totalCharacter
     total.textContent = totalWords
+    character.textContent = totalCharacter
 })
